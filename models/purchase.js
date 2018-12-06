61588b3a2eeb38cc786b9e336f324b7d9d7764e7
ShamSZ <sham.szoda@gmail.com>
2018-12-06 11:27:20 +0000

    Created purchase model

---
+++ models/purchase.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const purchaseSchema = mongoose.Schema({
+  product: { type: mongoose.Schema.ObjectId, ref: 'Product', required: true },
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  unitPrice: Number,
+  unitQuantity: Number,
+  status: { type: String, enum: ['processed'], default: 'processed' }
+}, { timestamps: true });
+
+purchaseSchema.virtual('totalPrice')
+  .get(function() {
+    return this.unitPrice * this.unitQuantity;
+  });
+
+purchaseSchema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('Purchase', purchaseSchema);
