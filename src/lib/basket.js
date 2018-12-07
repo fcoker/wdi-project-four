27badb430828d1c4d23923582f444eb14c0a0050
ShamSZ <sham.szoda@gmail.com>
2018-12-07 15:03:24 +0000

    Edited increment quantity in basket lib to only accept positive quantities.

--- src/lib/basket.js
+++ src/lib/basket.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getHeader } from './auth';
+import { getHeader, decodeToken } from './auth';
 
 export function createBasket() {
   localStorage.setItem('basket', '[]');
@@ -20,7 +20,7 @@ export function getItem(basket, itemId) {
 
 export function incrementQuantity(basket, itemId, quantity) {
   const item = getItem(basket, itemId);
-  item.unitQuantity = (item.unitQuantity || 0) + quantity;
+  item.unitQuantity =(item.unitQuantity || 0) + Math.abs(quantity);
   saveBasket(basket);
 }
 
@@ -68,7 +68,7 @@ export function checkout() {
   axios.post('/api/checkout', getBasket(), getHeader())
     .then(() => {
       createBasket();
-      this.props.history.push('/purchases');
+      this.props.history.push(`/users/${decodeToken().sub}`);
     });
 }
 
