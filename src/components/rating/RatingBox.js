5dfab593aa4a75a8ea62b6835685d78f7dfb8313
ShamSZ <sham.szoda@gmail.com>
2018-12-11 12:03:30 +0000

    Styled rating box. Changed rating limits to 1-5.

--- src/components/rating/RatingBox.js
+++ src/components/rating/RatingBox.js
@@ -28,18 +28,18 @@ class RatingBox extends React.Component {
     return (
       <section className="hero is-fullheight">
         <div className="box">
-          <div className="cont">
+          <div className="star-box">
             <div className="stars">
-              <form action="">
-                <input className="star star-5" value="1" id="star-5" onChange={this.handleChange} type="radio" name="ratingValue"/>
+              <form onSubmit={this.handleSubmit}>
+                <input className="star star-5" value="5" id="star-5" onChange={this.handleChange} type="radio" name="ratingValue"/>
                 <label className="star star-5" htmlFor="star-5"></label>
-                <input className="star star-4" value="2" id="star-4" onChange={this.handleChange} type="radio" name="ratingValue"/>
+                <input className="star star-4" value="4" id="star-4" onChange={this.handleChange} type="radio" name="ratingValue"/>
                 <label className="star star-4" htmlFor="star-4"></label>
                 <input className="star star-3" value="3" id="star-3" onChange={this.handleChange} type="radio" name="ratingValue"/>
                 <label className="star star-3" htmlFor="star-3"></label>
-                <input className="star star-2" value="4" id="star-2" onChange={this.handleChange} type="radio" name="ratingValue"/>
+                <input className="star star-2" value="2" id="star-2" onChange={this.handleChange} type="radio" name="ratingValue"/>
                 <label className="star star-2" htmlFor="star-2"></label>
-                <input className="star star-1" value="5" id="star-1" onChange={this.handleChange} type="radio" name="ratingValue"/>
+                <input className="star star-1" value="1" id="star-1" onChange={this.handleChange} type="radio" name="ratingValue"/>
                 <label className="star star-1" htmlFor="star-1"></label>
                 <div>
                   <button className="button">Submit rating</button>
