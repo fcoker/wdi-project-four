7a8669e0251f4cfd66a6cdb6275ac1bd4541b233
Rafa <rafaelruizbernal@gmail.com>
2018-12-12 21:22:46 +0000

    frontEnd testing

---
+++ test/client/products/Index_spec.js
@@ -0,0 +1,43 @@
+/* global describe,it */
+import React from 'react';
+import axios from 'axios';
+import sinon from 'sinon';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import ProductsIndex from '../../../src/components/products/Index';
+
+const testData =[
+  {
+    name: 'Red Dead Redemption II',
+    format: 'PS4',
+    genre: 'RPG',
+    images: '/assets/images/redDead.png',
+    video: 'https://www.youtube.com/embed/F63h3v9QV7w?controls=1',
+    unitPrice: 49.99,
+    description: 'America, 1899. The end of the wild west era has begun as lawmen hunt down the last remaining outlaw gangs. Those who will not surrender or succumb are killed. From the creators of Grand Theft Auto V and Red Dead Redemption, Red Dead Redemption 2 is an epic tale of life in America at the dawn of the modern age.',
+    releaseDate: '26/10/2018',
+    reviews: []
+  }
+];
+
+sinon.stub(axios, 'get')
+  .returns(Promise.resolve({ data: testData }));
+
+describe('Products Index', () => {
+  it('should show the correct number of Product Boxes', done => {
+    const component = shallow(<ProductsIndex />);
+    component.setState({ filteredProducts: testData });
+    expect(component.state()).to.have.property('filteredProducts');
+    expect(component.find('ProductBox').length).to.eq(testData.length);
+    done();
+  });
+
+  it('should have the correct name in each ProductBox', done => {
+    const component = shallow(<ProductsIndex />);
+    component.setState({ filteredProducts: testData });
+    component.find('ProductBox').forEach((box, i) => {
+      expect(box.props().product.name).to.eq(testData[i].name);
+    });
+    done();
+  });
+});
---
+++ test/client/products/ProductBox_spec.js
@@ -0,0 +1,30 @@
+/* global describe,it */
+import React from 'react';
+// import axios from 'axios';
+// import sinon from 'sinon';
+import { shallow} from 'enzyme';
+import { expect } from 'chai';
+import ProductBox from '../../../src/components/products/ProductBox';
+
+const testData =
+  {
+    name: 'Red Dead Redemption II',
+    format: 'PS4',
+    genre: 'RPG',
+    images: '/assets/images/redDead.png',
+    video: 'https://www.youtube.com/embed/F63h3v9QV7w?controls=1',
+    unitPrice: 49.99,
+    description: 'America, 1899. The end of the wild west era has begun as lawmen hunt down the last remaining outlaw gangs. Those who will not surrender or succumb are killed. From the creators of Grand Theft Auto V and Red Dead Redemption, Red Dead Redemption 2 is an epic tale of life in America at the dawn of the modern age.',
+    releaseDate: '26/10/2018',
+    reviews: []
+  };
+
+describe('Product', () => {
+  it('should show the correct product',  done => {
+    const component = shallow(<ProductBox product={testData}/>);
+    expect(component.find('img').length).to.eq(1);
+    expect(component.find('img').props().src).to.eq(testData.images);
+    console.log(component.debug());
+    done();
+  });
+});
