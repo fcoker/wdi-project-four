3a4975d6f24a738e41d9a1b4bed2982fd287802b
ShamSZ <sham.szoda@gmail.com>
2018-12-12 16:27:23 +0000

    Added styling to show image slider

--- src/components/products/showSlider/LeftArrow.js
+++ src/components/products/showSlider/LeftArrow.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function LeftArrow(props){
   return (
-    <div className="prevArrow arrow" onClick={props.goToPrevSlide}>
+    <div className="prevArrow arrow leftArrow" onClick={props.goToPrevSlide}>
       <i className="fa fa-arrow-left fa-2x" aria-hidden="true"></i>
     </div>
   );
--- src/components/products/showSlider/RightArrow.js
+++ src/components/products/showSlider/RightArrow.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function RightArrow(props){
   return (
-    <div className="nextArrow arrow" onClick={props.goToNextSlide}>
+    <div className="nextArrow arrow rightArrow" onClick={props.goToNextSlide}>
       <i className="fa fa-arrow-right fa-2x" aria-hidden="true"></i>
     </div>
   );
